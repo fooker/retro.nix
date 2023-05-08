0c223d8776c05d7d1a492d2a6da6d0235d861172
Dustin Frisch <fooker@lab.sh>
2023-05-08 18:15:32 +0200

    fixup! Clear screen before game launch

--- launcher/systems.nix
+++ launcher/systems.nix
@@ -16,20 +16,20 @@ let
     ${ncurses}/bin/tput civis
     ${ncurses}/bin/clear
 
-    exec ${emulators.${emulator}.command}
+    exec ${system.emulators.${emulator}.command}
   '';
 
 in writeText "emulationstation-systems.cfg" ''
   <systemList>
   ${concatMapStringsSep "\n" (system: with system; ''
     <system>
-      <name>${id}</name>
-      <fullname>${fullName}</fullname>
-      <path>~/roms/${id}</path>
-      <extension>${concatMapStringsSep " " (ext: ".${ext}") fileExtensions}</extension>
-      <command>${command system defaultEmulator}</command>
-      <platform>${id}</platform>
-      <theme>${id}</theme>
+      <name>${system.id}</name>
+      <fullname>${system.fullName}</fullname>
+      <path>~/roms/${system.id}</path>
+      <extension>${concatMapStringsSep " " (ext: ".${ext}") system.fileExtensions}</extension>
+      <command>${command system system.defaultEmulator}</command>
+      <platform>${system.id}</platform>
+      <theme>${system.id}</theme>
     </system>
   '') (filter (system: system.enable) (attrValues config.systems))}
   </systemList>
